ea480a2f32d7fd0764ffafc028d19f35c576eb68
akdo <akdoganahmet42@gmail.com>
2024-11-04 14:37:26 +0300

    First commit

---
+++ findprimes.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int numeros1, i, j, control=1;
+	cout << "Enter a number: ";
+	cin >> numeros1;
+	for (i = 2; i <= numeros1; i++) {
+		control = 1;
+		for (j = 2; j <= sqrt(i); j++) {
+			if (i % j== 0) {
+				control = 2;
+				break;
+			}
+
+		}
+		if (control == 1)
+			cout << i << " ";
+	
+	}
+	return 0;
+}
